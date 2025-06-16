e2cdad7c5d56a111a4caf83fcb7795accf5db041
jmxr <huanghexiang@mindflow.com.cn>
2025-06-16 20:40:46 +0800

    feat: 更新SpeedInsights组件，添加endpoint属性以支持Vercel性能监控

--- app/layout.tsx
+++ app/layout.tsx
@@ -136,7 +136,7 @@ export default async function RootLayout({
             <Toaster />
           </ThemeProvider>
         </LocaleProvider>
-        <SpeedInsights />
+        <SpeedInsights endpoint='_vercel/speed-insights/vitals' />
         <Analytics />
         <SEOMonitor />
         {process.env.NODE_ENV === 'development' && (
